8dcba8dd41e3fff4a62a3b487c2567d09d16a063
Rose Mullan <rfmullan@gmail.com>
2024-10-17 21:09:23 +0100

    add topics endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,28 @@
+const db = require("../db/connection")
+const app = require("../app.js")
+const seed = require("../db/seeds/seed.js")
+const data = require("../db/data/test-data")
+const request = require("supertest")
 
+beforeEach(() => { 
+    return seed(data)
+})
+
+afterAll(() => { 
+    db.end()
+})
 
 describe("/api/topics", () => { 
     test("responds with an array of topics", () => { 
-
+        return request(app)
+            .get('/api/topics')
+            .expect(200)
+            .then(({ body: { topics } }) => { 
+                expect(topics.length).not.toBe(0)
+                topics.forEach((topic) => { 
+                    expect(typeof topic.slug).toBe("string")
+                    expect(typeof topic.description).toBe("string")
+                })
+            })
     })
 })
--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const getTopics = require("./controllers/topics.controllers");
 const app = express();
 
 app.get("/api", (request, response) => { 
     response.status(200).send({message: "fine"})
 });
+
+app.get("/api/topics", getTopics)
 
 module.exports = app
