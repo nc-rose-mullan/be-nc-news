cbd227cdc3d4c37a9af43ea263d313494767bd72
Rose Mullan <rfmullan@gmail.com>
2024-10-17 22:03:41 +0100

    add another test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,17 +12,20 @@ afterAll(() => {
     db.end()
 })
 
-describe("GET: /api/topics", () => { 
+describe("GET: /api/topics", () => {
     test("responds with an array of topics", () => {
         return request(app)
             .get("/api/topics")
             .expect(200)
-            .then(({ body: { topics } }) => { 
-                expect(topics.length).not.toBe(0)
-                topics.forEach((topic) => { 
-                    expect(typeof topic.slug).toBe("string")
-                    expect(typeof topic.description).toBe("string")
-                })
-            })
+            .then(({ body: { topics } }) => {
+                expect(topics.length).not.toBe(0);
+                topics.forEach((topic) => {
+                    expect(typeof topic.slug).toBe("string");
+                    expect(typeof topic.description).toBe("string");
+                });
+            });
     });
+    test("hello new test", () => { 
+        expect(2+2).toBe(4)
+    })
 })
